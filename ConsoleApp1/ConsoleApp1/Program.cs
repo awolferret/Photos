2e0234f54831bb4d1e5f6128188de2373ec7ed52
awolferret <104518827+awolferret@users.noreply.github.com>
2022-04-28 20:59:49 +0300

    1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int photos = 52;
+            int rowNumber = 3;
+            int row = photos / rowNumber;
+            int photosTooMuch = photos % rowNumber;
+            Console.WriteLine($"полных рядов{row}");
+            Console.WriteLine($"сверх меры{photosTooMuch}");
+        }
+    }
+}
