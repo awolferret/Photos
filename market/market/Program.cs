f1e76fd15db0e06ebaa2cc0e2b9780333985cf46
awolferret <104518827+awolferret@users.noreply.github.com>
2022-04-28 21:24:58 +0300

    1

---
+++ market/market/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace market
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Сколько у вас золота?");
+            int gold = Convert.ToInt32(Console.ReadLine());
+            int priceForCristal = 13;
+            int possibleToBuy = gold / priceForCristal;
+            Console.WriteLine($"Вы можете купить {possibleToBuy}") ;
+            Console.WriteLine($"Сколько вы хотите купить?");
+            int buy = Convert.ToInt32(Console.ReadLine());
+            int goldRemains = gold - (buy * priceForCristal);
+            Console.WriteLine($"Вы купили {buy} кристалов и у вас осталось {goldRemains} рублей"); 
+        }
+    }
+}
